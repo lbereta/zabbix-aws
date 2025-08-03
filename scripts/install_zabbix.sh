58380958313e7f76a3f997ab513e4435836829a3
Lucas Schroder Bereta <lucas.bereta@safeweb.com.br>
2025-08-03 20:30:01 -0300

    :building_construction: Change DB

--- scripts/install_zabbix.sh
+++ scripts/install_zabbix.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
+
+set -e
+
+# Atualiza pacotes
 apt update -y
 apt upgrade -y
-apt install -y curl gnupg2 apache2 mysql-server php php-mysql libapache2-mod-php unzip wget
+
+# Instala dependências
+apt install -y curl gnupg2 apache2 mariadb-server php php-mysql libapache2-mod-php unzip wget
 
 # Adiciona o repositório Zabbix 7.0 LTS
 wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-1+ubuntu22.04_all.deb
 dpkg -i zabbix-release_7.0-1+ubuntu22.04_all.deb
 apt update -y
 
-# Instala Zabbix
+# Instala o Zabbix
 apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
 
-# Configura banco de dados
+# Inicializa o banco de dados
 mysql -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
-mysql -e "create user zabbix@localhost identified by 'zabbixpass';"
-mysql -e "grant all privileges on zabbix.* to zabbix@localhost;"
+mysql -e "create user 'zabbix'@'localhost' identified by 'zabbixpass';"
+mysql -e "grant all privileges on zabbix.* to 'zabbix'@'localhost';"
+mysql -e "flush privileges;"
+
 zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -pzabbixpass zabbix
 
-# Configura Zabbix
+# Configura o Zabbix server
 sed -i 's/^# DBPassword=/DBPassword=zabbixpass/' /etc/zabbix/zabbix_server.conf
 
-# Inicia serviços
-systemctl enable zabbix-server zabbix-agent apache2 mysql
-systemctl restart zabbix-server zabbix-agent apache2
+# Inicia os serviços
+systemctl enable zabbix-server zabbix-agent apache2 mariadb
+systemctl restart zabbix-server zabbix-agent apache2 mariadb
