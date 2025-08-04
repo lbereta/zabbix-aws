2342628ca41f57bfeefeeede07f4720ebbe12c06
Lucas Schroder Bereta <lucas.bereta@safeweb.com.br>
2025-08-03 21:10:25 -0300

    :construction_worker: Add destroy workflow

--- scripts/install_zabbix.sh
+++ scripts/install_zabbix.sh
@@ -2,32 +2,31 @@
 
 set -e
 
-# Atualiza pacotes
 apt update -y
 apt upgrade -y
 
-# Instala dependências
+
 apt install -y curl gnupg2 apache2 mariadb-server php php-mysql libapache2-mod-php unzip wget
 
-# Adiciona o repositório Zabbix 7.0 LTS
 wget https://repo.zabbix.com/zabbix/7.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_7.0-1+ubuntu22.04_all.deb
 dpkg -i zabbix-release_7.0-1+ubuntu22.04_all.deb
 apt update -y
 
-# Instala o Zabbix
-apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent
+apt install -y zabbix-server-mysql zabbix-frontend-php zabbix-apache-conf zabbix-agent zabbix-sql-scripts
 
-# Inicializa o banco de dados
 mysql -e "create database zabbix character set utf8mb4 collate utf8mb4_bin;"
 mysql -e "create user 'zabbix'@'localhost' identified by 'zabbixpass';"
 mysql -e "grant all privileges on zabbix.* to 'zabbix'@'localhost';"
 mysql -e "flush privileges;"
 
-zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -pzabbixpass zabbix
+until mysqladmin ping -uzabbix -pzabbixpass --silent; do
+    echo "Aguardando MariaDB iniciar..."
+    sleep 2
+done
 
-# Configura o Zabbix server
+zcat /usr/share/zabbix-sql-scripts/mysql/server.sql.gz | mysql -uzabbix -pzabbixpass zabbix
+
 sed -i 's/^# DBPassword=/DBPassword=zabbixpass/' /etc/zabbix/zabbix_server.conf
 
-# Inicia os serviços
 systemctl enable zabbix-server zabbix-agent apache2 mariadb
 systemctl restart zabbix-server zabbix-agent apache2 mariadb
